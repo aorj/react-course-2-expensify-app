4255bfc0fba56f453736ee48239b38e151cd2ee8
aorJ <siriluckj7@gmail.com>
2020-12-14 20:30:09 +0700

    remove unused component

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpense from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
