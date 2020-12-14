f04aeeca5d6f0938a318794fa3b9db96d36dd74a
aorJ <siriluckj7@gmail.com>
2020-12-14 21:21:14 +0700

    add firebase

--- src/app.js
+++ src/app.js
@@ -8,24 +8,14 @@ import getVisibleExpense from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-
+import './firebase/firebase';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 999 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 100999 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
-
 
 ReactDOM.render(jsx, document.getElementById('app'));
