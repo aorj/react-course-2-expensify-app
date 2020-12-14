1788fd0ad97726548f662e09c2c9eaee13982484
aorJ <siriluckj7@gmail.com>
2020-12-14 20:31:12 +0700

    Add expenses summary

---
+++ src/selectors/expenses-total.js
@@ -0,0 +1,12 @@
+export default (expenses) => {
+  if (expenses.length === 0) {
+    return 0
+  } else {
+    // const amounts = expenses.map((expense) => expense.amount);
+    // const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    // return amounts.reduce(reducer);
+    return expenses
+    .map((expense) => expense.amount)
+    .reduce((sum, currentValue) => sum + currentValue, 0);
+  }
+};
