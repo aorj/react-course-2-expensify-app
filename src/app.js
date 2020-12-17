e22f2e4b75274521a23492a3bff2be563ff87e7b
aorJ <siriluckj7@gmail.com>
2020-12-17 13:55:53 +0700

    remove unused

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import getVisibleExpense from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
