72548184aada0aedfb8c9c1fdcb623c2600c876e
aorJ <siriluckj7@gmail.com>
2020-12-17 10:05:51 +0700

    fix webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
-const { webpack } = require('webpack');
+const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
